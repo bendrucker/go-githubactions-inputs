0177230c8a952f2561d36f011538611e05be9905
Ben Drucker <bvdrucker@gmail.com>
2021-09-02 14:25:58 -0700

    cut tests down to 1 field

--- struct_test.go
+++ struct_test.go
@@ -14,203 +14,75 @@ func TestDecode(t *testing.T) {
 		check  func(interface{})
 	}{
 		{
-			name: "string",
-			inputs: map[string]string{
-				"foo": "bar",
-				"baz": "qux",
-			},
-			value: &struct {
-				Foo string
-				Baz string
-			}{},
+			name:   "string",
+			inputs: map[string]string{"foo": "bar"},
+			value:  &struct{ Foo string }{},
 			check: func(v interface{}) {
-				s := v.(*struct {
-					Foo string
-					Baz string
-				})
-
-				assert.Equal(t, "bar", s.Foo)
-				assert.Equal(t, "qux", s.Baz)
+				assert.Equal(t, "bar", v.(*struct{ Foo string }).Foo)
 			},
 		},
 		{
-			name: "int",
-			inputs: map[string]string{
-				"foo": "1",
-				"baz": "2",
-			},
-			value: &struct {
-				Foo int
-				Baz int
-			}{},
+			name:   "int",
+			inputs: map[string]string{"foo": "1"},
+			value:  &struct{ Foo int }{},
 			check: func(v interface{}) {
-				s := v.(*struct {
-					Foo int
-					Baz int
-				})
-
-				assert.Equal(t, 1, s.Foo)
-				assert.Equal(t, 2, s.Baz)
+				assert.Equal(t, 1, v.(*struct{ Foo int }).Foo)
 			},
 		},
 		{
-			name: "int64",
-			inputs: map[string]string{
-				"foo": "1",
-				"baz": "2",
-			},
-			value: &struct {
-				Foo int64
-				Baz int64
-			}{},
+			name:   "int64",
+			inputs: map[string]string{"foo": "1"},
+			value:  &struct{ Foo int64 }{},
 			check: func(v interface{}) {
-				s := v.(*struct {
-					Foo int64
-					Baz int64
-				})
-
-				assert.Equal(t, int64(1), s.Foo)
-				assert.Equal(t, int64(2), s.Baz)
+				assert.Equal(t, int64(1), v.(*struct{ Foo int64 }).Foo)
 			},
 		},
 		{
-			name: "float",
-			inputs: map[string]string{
-				"foo": "1.1",
-				"baz": "2.2",
-			},
-			value: &struct {
-				Foo float64
-				Baz float64
-			}{},
+			name:   "float",
+			inputs: map[string]string{"foo": "1.1"},
+			value:  &struct{ Foo float64 }{},
 			check: func(v interface{}) {
-				s := v.(*struct {
-					Foo float64
-					Baz float64
-				})
-
-				assert.Equal(t, 1.1, s.Foo)
-				assert.Equal(t, 2.2, s.Baz)
+				assert.Equal(t, 1.1, v.(*struct{ Foo float64 }).Foo)
 			},
 		},
 		{
-			name: "bool",
-			inputs: map[string]string{
-				"foo": "true",
-				"baz": "false",
-			},
-			value: &struct {
-				Foo bool
-				Baz bool
-			}{},
+			name:   "bool",
+			inputs: map[string]string{"foo": "true"},
+			value:  &struct{ Foo bool }{},
 			check: func(v interface{}) {
-				s := v.(*struct {
-					Foo bool
-					Baz bool
-				})
-
-				assert.True(t, s.Foo)
-				assert.False(t, s.Baz)
-			},
-		},
-		// {
-		// 	name: "struct with pointer",
-		// 	inputs: map[string]string{
-		// 		"foo": "bar",
-		// 		"baz": "qux",
-		// 	},
-		// 	value: &struct {
-		// 		Foo *string
-		// 		Baz *string
-		// 	}{},
-		// 	check: func(v interface{}) {
-		// 		s := v.(*struct {
-		// 			Foo *string
-		// 			Baz *string
-		// 		})
-
-		// 		assert.Equal(t, "bar", *s.Foo)
-		// 		assert.Equal(t, "qux", *s.Baz)
-		// 	},
-		// },
-		{
-			name: "string slice",
-			inputs: map[string]string{
-				"foo": "bar1, bar2",
-				"baz": "qux1, qux2",
-			},
-			value: &struct {
-				Foo []string
-				Baz []string
-			}{},
-			check: func(v interface{}) {
-				s := v.(*struct {
-					Foo []string
-					Baz []string
-				})
-
-				assert.Equal(t, []string{"bar1", "bar2"}, s.Foo)
-				assert.Equal(t, []string{"qux1", "qux2"}, s.Baz)
+				assert.True(t, v.(*struct{ Foo bool }).Foo)
 			},
 		},
 		{
-			name: "int slice",
-			inputs: map[string]string{
-				"foo": "1, 2",
-				"baz": "3, 4",
-			},
-			value: &struct {
-				Foo []int
-				Baz []int
-			}{},
+			name:   "string slice",
+			inputs: map[string]string{"foo": "bar1, bar2"},
+			value:  &struct{ Foo []string }{},
 			check: func(v interface{}) {
-				s := v.(*struct {
-					Foo []int
-					Baz []int
-				})
-
-				assert.Equal(t, []int{1, 2}, s.Foo)
-				assert.Equal(t, []int{3, 4}, s.Baz)
+				assert.Equal(t, []string{"bar1", "bar2"}, v.(*struct{ Foo []string }).Foo)
 			},
 		},
 		{
-			name: "int64 slice",
-			inputs: map[string]string{
-				"foo": "1, 2",
-				"baz": "3, 4",
-			},
-			value: &struct {
-				Foo []int64
-				Baz []int64
-			}{},
+			name:   "int slice",
+			inputs: map[string]string{"foo": "1, 2"},
+			value:  &struct{ Foo []int }{},
 			check: func(v interface{}) {
-				s := v.(*struct {
-					Foo []int64
-					Baz []int64
-				})
-
-				assert.Equal(t, []int64{1, 2}, s.Foo)
-				assert.Equal(t, []int64{3, 4}, s.Baz)
+				assert.Equal(t, []int{1, 2}, v.(*struct{ Foo []int }).Foo)
 			},
 		},
 		{
-			name: "float64 slice",
-			inputs: map[string]string{
-				"foo": "1.1, 2.2",
-				"baz": "3.3, 4.4",
-			},
-			value: &struct {
-				Foo []float64
-				Baz []float64
-			}{},
+			name:   "int64 slice",
+			inputs: map[string]string{"foo": "1, 2"},
+			value:  &struct{ Foo []int64 }{},
 			check: func(v interface{}) {
-				s := v.(*struct {
-					Foo []float64
-					Baz []float64
-				})
-
-				assert.Equal(t, []float64{1.1, 2.2}, s.Foo)
-				assert.Equal(t, []float64{3.3, 4.4}, s.Baz)
+				assert.Equal(t, []int64{1, 2}, v.(*struct{ Foo []int64 }).Foo)
+			},
+		},
+		{
+			name:   "float64 slice",
+			inputs: map[string]string{"foo": "1.1, 2.2"},
+			value:  &struct{ Foo []float64 }{},
+			check: func(v interface{}) {
+				assert.Equal(t, []float64{1.1, 2.2}, v.(*struct{ Foo []float64 }).Foo)
 			},
 		},
 	}
