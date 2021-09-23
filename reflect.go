2798de63dd28abccec3dc66a634473dc3a885aeb
Ben Drucker <bvdrucker@gmail.com>
2021-09-23 13:54:58 -0700

    extract reflectSlice

--- reflect.go
+++ reflect.go
@@ -18,22 +18,26 @@ func reflectValue(name string, wantType reflect.Type) (reflect.Value, error) {
 	case reflect.Bool:
 		return reflect.ValueOf(Bool(name)), nil
 	case reflect.Slice:
-		switch wantType.Elem().Kind() {
-		case reflect.String:
-			return reflect.ValueOf(StringSlice(name)), nil
-		case reflect.Int:
-			v, err := IntSlice(name)
-			return reflect.ValueOf(v), err
-		case reflect.Int64:
-			v, err := Int64Slice(name)
-			return reflect.ValueOf(v), err
-		case reflect.Float64:
-			v, err := Float64Slice(name)
-			return reflect.ValueOf(v), err
-		default:
-			panic("unsupported slice type")
-		}
+		return reflectSlice(name, wantType.Elem())
 	default:
 		panic("unsupported type")
+	}
+}
+
+func reflectSlice(name string, elemType reflect.Type) (reflect.Value, error) {
+	switch elemType.Kind() {
+	case reflect.String:
+		return reflect.ValueOf(StringSlice(name)), nil
+	case reflect.Int:
+		v, err := IntSlice(name)
+		return reflect.ValueOf(v), err
+	case reflect.Int64:
+		v, err := Int64Slice(name)
+		return reflect.ValueOf(v), err
+	case reflect.Float64:
+		v, err := Float64Slice(name)
+		return reflect.ValueOf(v), err
+	default:
+		panic("unsupported slice type")
 	}
 }
